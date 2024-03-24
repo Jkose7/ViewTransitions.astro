3ead704441990cfe28183ee02fb8a7ebc8ef7bb8
Jkose7 <ojosema10@gmail.com>
2024-03-24 16:53:27 -0500

    Page 404

---
+++ src/data/albums.js
@@ -0,0 +1,33 @@
+export const Albums = [
+    {
+        id: "in-rainbowns",
+        title: "In Rainbowns",
+        img: "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/dd/50/c7/dd50c790-99ac-d3d0-5ab8-e3891fb8fd52/634904032463.png/1200x1200bb.jpg",
+    },
+    {
+        id: "el-jardin-de-los-presentes",
+        title: "El Jardin De los Presentes",
+        img: "https://jazzrocksoul.com/wp-content/uploads/2018/01/Invisible-El-Jard%C3%ADn-de-los-Presentes.jpg",
+    },
+    {
+        id: "origin-of-symmetry",
+        title: "Origin Of Symmetry",
+        img: "https://upload.wikimedia.org/wikipedia/en/3/35/Muse_-_Origin_of_Symmetry_cover_art.png",
+    },
+    {
+        id: "whats-goin-on",
+        title: "What's Going On",
+        img: "https://upload.wikimedia.org/wikipedia/en/8/84/MarvinGayeWhat%27sGoingOnalbumcover.jpg",
+    },
+    {
+        id: "white-pony",
+        title: "White-Pony",
+        img: "https://http2.mlstatic.com/D_NQ_NP_959047-MLU70545031083_072023-O.webp",
+    },
+    {
+        id: "chet",
+        title: "Chet",
+        img: "https://upload.wikimedia.org/wikipedia/en/1/1d/Chet_%28album%29.jpg",
+    },
+    
+];
