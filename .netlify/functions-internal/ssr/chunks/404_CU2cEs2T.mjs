b63d7942723b12b75c0c00cd80bceae5bdc6940e
Jkose7 <ojosema10@gmail.com>
2024-05-17 12:39:11 -0500

    fix problem to deploy

--- .netlify/functions-internal/ssr/chunks/404_CU2cEs2T.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/404_B8BzK37a.mjs').then(n => n._);
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/_album__7V7lUhcn.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/_album__BsSWI2WK.mjs').then(n => n._);
-
-export { page };
